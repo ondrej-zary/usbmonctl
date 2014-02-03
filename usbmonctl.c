4085cdf3f2a2a584c60171bdd4cec2006a447058
Ondrej Zary <zary@gsystems.sk>
2014-02-03 16:17:21 +0100

    add #include <linux/types.h> to fix compilation with older kernels

--- usbmonctl.c
+++ usbmonctl.c
@@ -6,12 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <linux/types.h>
 #include <linux/hiddev.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define VERSION "1.0"
+#define VERSION "1.1"
 
 #define HID_USAGE_PAGE		0xffff0000
 
